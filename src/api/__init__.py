b0cba3e98c3109baaad13a4524df35901f0c9341
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2025-02-17 22:44:45 -0300

    docs: ideias to arch

---
+++ src/api/__init__.py
@@ -0,0 +1,14 @@
+"""
+# API package
+
+Why do we need an API package?
+
+- This package contains all the API modules used in the application.
+- The API modules are defined using Classes.
+- The API modules are defined in the files in this package.
+
+## API Modules
+
+- base.py: Contains the base API module class.
+
+"""
---
+++ src/schemas/__init__.py
@@ -0,0 +1,19 @@
+"""
+# Application Schemas
+
+Why do we need schemas?
+
+- This package contains all the schemas used in the application.
+- Schemas are used to validate the data that is being sent to the application.
+- Schemas are also used to serialize the data that is being sent to the application.
+- Schemas are created using Pydantic.
+- The base schema class is defined in the base.py file.
+- The schemas are defined in the files in this package.
+- The schemas are imported in the routes and services.
+
+## Schemas
+
+- base.py: Contains the base schema class.
+- ollama.py: Contains the schema for the Ollama model.
+
+"""
